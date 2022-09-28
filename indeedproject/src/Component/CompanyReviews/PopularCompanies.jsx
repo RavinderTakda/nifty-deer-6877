cef59a48bacdf03dddcc0228c4cfa07fd311b3bf
AdityaKothari1 <105914134+AdityaKothari1@users.noreply.github.com>
2022-09-29 02:07:08 +0530

    Allupdated

--- indeedproject/src/Component/CompanyReviews/PopularCompanies.jsx
+++ indeedproject/src/Component/CompanyReviews/PopularCompanies.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import { Box, Flex, SimpleGrid, Text, Image } from "@chakra-ui/react";
-import { StarIcon } from "@chakra-ui/icons";
+// import { StarIcon } from "@chakra-ui/icons";
 
 const PopularCompanies = () => {
   const [companies, setCompanies] = useState([]);
--- indeedproject/src/Component/Pages/Allroutes.jsx
+++ indeedproject/src/Component/Pages/Allroutes.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+// import { SignUp } from '../Authentication/SignUp'
+import CompanyReviews from '../CompanyReviews/CompanyReviews'
+import Salary from '../SalaryGuide/Salary'
+import { MainCom } from './MainCom'
 
 export const Allroutes = () => {
   return (
     <div>
-        
+        <Routes>
+          {/* <Route path="/signup" element={<SignUp/>}  /> */}
+          {/* {/* <Route path='/' element={<MainCom/>}/> */}
+          <Route path='/companyreview' element={<CompanyReviews/>}/>
+          <Route path="/salary" element={<Salary/>}/>
+        </Routes>
     </div>
   )
 }
