5fa140601515c5b84a38dd456285ac50aab69cae
Abdul Rub <arub0419@gmail.com>
2022-09-29 11:43:51 +0530

    SignupSetup

--- indeedproject/src/Component/Pages/Allroutes.jsx
+++ indeedproject/src/Component/Pages/Allroutes.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-// import { SignUp } from '../Authentication/SignUp'
+import { SignUp } from '../Authentication/SignUp'
 import CompanyReviews from '../CompanyReviews/CompanyReviews'
 import Salary from '../SalaryGuide/Salary'
-import { MainCom } from './MainCom'
 
 export const Allroutes = () => {
   return (
     <div>
         <Routes>
-          {/* <Route path="/signup" element={<SignUp/>}  /> */}
-          {/* {/* <Route path='/' element={<MainCom/>}/> */}
+          <Route path="/signup" element={<SignUp/>}  />
           <Route path='/companyreview' element={<CompanyReviews/>}/>
           <Route path="/salary" element={<Salary/>}/>
         </Routes>
