2f33da5fd3075d5e9811f83d6ddf3710392ec504
Abdul Rub <arub0419@gmail.com>
2022-09-29 23:24:01 +0530

    login

--- indeedproject/src/Component/Pages/Allroutes.jsx
+++ indeedproject/src/Component/Pages/Allroutes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { Login } from '../Authentication/Login'
 import { SignUp } from '../Authentication/SignUp'
 import CompanyReviews from '../CompanyReviews/CompanyReviews'
 import Salary from '../SalaryGuide/Salary'
@@ -9,6 +10,7 @@ export const Allroutes = () => {
     <div>
         <Routes>
           <Route path="/signup" element={<SignUp/>}  />
+          <Route path='/login' element={<Login/>}/>
           <Route path='/companyreview' element={<CompanyReviews/>}/>
           <Route path="/salary" element={<Salary/>}/>
         </Routes>
---
+++ indeedproject/src/Firebase.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth,GoogleAuthProvider,signInWithPopup} from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBm45e-9sL8IVEHo6P4eQNBKYaihRH9ZzE",
+  authDomain: "indeedauth.firebaseapp.com",
+  projectId: "indeedauth",
+  storageBucket: "indeedauth.appspot.com",
+  messagingSenderId: "489087098112",
+  appId: "1:489087098112:web:f2f9797cf5daed2799a7e5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth()
+
+// const provider = new GoogleAuthProvider();
+// export const signInWithGoogle=()=>{
+//     signInWithPopup(auth,provider)
+//     .then((res)=>{
+//         console.log(res.data)
+//         const email = res.user.email;
+        
+//         localStorage.setItem("email", email)
+
+//     }).catch((err)=>{
+//         console.log(err)
+//     })
+    
+    
+// }
