a5799a54fd36d3c6ccbedbea9d6d7a6e70b19f65
Abdul Rub <arub0419@gmail.com>
2022-10-02 00:50:56 +0530

    reloadfix

--- indeedproject/src/App.js
+++ indeedproject/src/App.js
@@ -1,29 +1,26 @@
-// <<<<<<< HEAD
-
-// import './App.css';
-// import Salary from './SalaryGuide/Salary';
-
-// function App() {
-//   return (
-//     <div className="App">
-//            <Salary/>
-// =======
-
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
-import { ApplyJobForm } from "./Component/FindJob/ApplyJobForm";
-import { HomePage } from "./Component/FindJob/HomePage";
-import { Searchbox } from "./Component/FindJob/Searchbox";
-import { Allroutes } from "./Component/Pages/Allroutes";
-
 import { MainCom } from "./Component/Pages/MainCom";
-
+import { setUser } from "./Component/Redux/Authentication/action";
+import { auth } from "./Firebase";
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    auth.onAuthStateChanged((authUser)=>{
+      if(authUser){
+        dispatch(setUser(authUser));
+      }else{
+        dispatch(setUser(null));
+      }
+    })
+  },[dispatch])
+
+
   return (
     <div>
-     
         <MainCom />
-        
-    {/* <ApplyJobForm></ApplyJobForm> */}
     </div>
   );
 }
